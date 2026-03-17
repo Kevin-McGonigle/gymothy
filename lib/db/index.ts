06942cd4576d971720f63353eefe17965d29ec87
Kevin McGonigle <kevin@bogpeople.com>
2026-03-17 11:07:11 +0000

    fix drizzle config

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,13 +1,9 @@
-import { createClient } from "@libsql/client";
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
-import * as schema from "./schema";
 
-const url = process.env.TURSO_DATABASE_URL!;
-
-if (!url) {
-  throw new Error("TURSO_DATABASE_URL is not defined");
-}
-
-const client = createClient({ url });
-
-export const db = drizzle(client, { schema });
+export const db = drizzle({
+  connection: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+});
